0dd12074e441756eb8b3b832dc3798ac6c016b66
aishu-b123 <166806479+aishu-b123@users.noreply.github.com>
2024-05-31 19:14:35 +0530

    Delete project/templates/index.js

--- project/templates/index.js
+++
@@ -1,5 +0,0 @@
-let homeDivEle=document.getElementById("main_home");
-let searchbtnEle=document.getElementById("searchbtn");
-searchbtnEle.onclick=function(){
-    homeDivEle.classList.add("d-none");
-}
